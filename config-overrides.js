89f4248d5a1152403b9611fc5025118101aa0208
Madis Abel <mabel@twilio.com>
2018-07-11 17:36:06 +0300

    Working build of a sample app  (#1)

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,29 @@
 const STATICS_PATH = "assets/";
 const STATICS_SEARCH = "static/";
 
+const suppressEPIPE = (stream) => {
+    if (!stream) return;
+    const onError = (err) => {
+        if (err.code === "EPIPE" || err.errno === /*EPIPE*/32) {
+            return process.exit(0);
+        }
+
+        const listenerCount = stream.listenerCount ? stream.listenerCount("error") : stream.listeners("error").length;
+        if (listenerCount == 1) {
+            stream.removeListener('error', onError);
+            stream.emit('error', err);
+            stream.on('error', onError);
+        }
+    };
+
+    stream.on('error', onError);
+};
+
+// suppress EPIPE errors for CI integration, make process exit with code 0
+if (typeof process !== 'undefined') {
+    suppressEPIPE(process.stdout);
+}
+
 // Replace 'static/' folder references in default react scripts to use /assets folder.
 const replaceIfStaticPath = (path) => {
     if (path.indexOf(STATICS_SEARCH) >= 0) {
@@ -16,10 +39,10 @@ const replaceIfStaticPath = (path) => {
 }
 
 const processConfig = (configObj) => {
-    if (typeof(configObj) === "object" && !!configObj) {
+    if (typeof (configObj) === "object" && !!configObj) {
         Object.keys(configObj).forEach(key => {
             const val = configObj[key];
-            if (typeof(val) === "string") {
+            if (typeof (val) === "string") {
                 configObj[key] = replaceIfStaticPath(val);
             }
             else {
