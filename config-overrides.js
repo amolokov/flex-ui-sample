708e447ce10d2a778d82e41306789fb3a73a9a11
Madis Abel <mabel@twilio.com>
2018-07-12 13:10:52 +0300

    Cleaning up error handling

--- config-overrides.js
+++ config-overrides.js
@@ -4,15 +4,15 @@ const STATICS_SEARCH = "static/";
 const suppressEPIPE = (stream) => {
     if (!stream) return;
     const onError = (err) => {
-        if (err.code === "EPIPE" || err.errno === /*EPIPE*/32) {
+        if (err.code === "EPIPE" || err.errno === 32/*EPIPE*/) {
             return process.exit(0);
         }
 
         const listenerCount = stream.listenerCount ? stream.listenerCount("error") : stream.listeners("error").length;
         if (listenerCount == 1) {
-            stream.removeListener('error', onError);
-            stream.emit('error', err);
-            stream.on('error', onError);
+            stream.removeListener("error", onError);
+            stream.emit("error", err);
+            stream.on("error", onError);
         }
     };
 
@@ -20,47 +20,9 @@ const suppressEPIPE = (stream) => {
 };
 
 // suppress EPIPE errors for CI integration, make process exit with code 0
-// if (typeof process !== 'undefined') {
-//     suppressEPIPE(process.stdout);
-// }
-
-var eoepipe = function eoepipeit(S/*:events$EventEmitter*/, bail/*:?()=>any*/) {
-	if(!S || !S.on) return;
-	if(!bail && typeof process !== 'undefined') bail = process.exit;
-	var eoe = function eoeit(err/*:ErrnoError*/) {
-		if(err.code === 'EPIPE' || err.errno === /*EPIPE*/32) { if(bail) bail(0); else return; }
-		var cnt = S.listenerCount ? S.listenerCount('error') : S.listeners('error').length;
-		if(cnt == 1) {
-			S.removeListener('error', eoe);
-			S.emit('error', err);
-			S.on('error', eoe);
-		}
-	};
-	S.on('error', eoe);
-};
-
-//if(typeof module !== 'undefined') module.exports = eoepipe;
-// if(typeof process !== 'undefined') eoepipe(process.stdout);
-
-function epipeBomb(stream, callback) {
-    if (stream == null) stream = process.stdout
-    if (callback == null) callback = process.exit
-
-    function epipeFilter(err) {
-      if (err.code === 'EPIPE') return callback()
-
-      // If there's more than one error handler (ie, us),
-      // then the error won't be bubbled up anyway
-      if (stream.listeners('error').length <= 1) {
-        stream.removeAllListeners()     // Pretend we were never here
-        stream.emit('error', err)       // Then emit as if we were never here
-        stream.on('error', epipeFilter) // Then reattach, ready for the next error!
-      }
-    }
-
-    stream.on('error', epipeFilter)
+if (typeof process !== 'undefined') {
+    suppressEPIPE(process.stdout);
 }
-epipeBomb(process.stdout, process.exit);
 
 // Replace 'static/' folder references in default react scripts to use /assets folder.
 const replaceIfStaticPath = (path) => {
